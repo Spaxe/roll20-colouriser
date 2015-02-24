f094ba491012eccb714c5efae71737054da61f01
Xavier Ho <contact@xavierho.com>
2015-02-24 15:16:14 +1100

    Version 1.1 fixed events injection

--- roll20-colourise.js
+++ roll20-colourise.js
@@ -1,103 +1,109 @@
-function hashCode(str) {
-    var hash = 0;
-    if (str.length == 0) return hash;
-    for (i = 0; i < str.length; i++) {
-        char = str.charCodeAt(i);
-        hash = ((hash<<7)-hash)+char;
-        hash = hash & hash; // Convert to 32bit integer
+var actualCode = '(' + function() {
+  function hashCode(str) {
+      var hash = 0;
+      if (str.length == 0) return hash;
+      for (i = 0; i < str.length; i++) {
+          char = str.charCodeAt(i);
+          hash = ((hash<<7)-hash)+char;
+          hash = hash & hash; // Convert to 32bit integer
+      }
+      return hash;
+  }
+
+  function hsvToRgb(h, s, v) {
+    var r, g, b;
+    var i;
+    var f, p, q, t;
+
+    // Make sure our arguments stay in-range
+    h = Math.max(0, Math.min(360, h));
+    s = Math.max(0, Math.min(100, s));
+    v = Math.max(0, Math.min(100, v));
+
+    // We accept saturation and value arguments from 0 to 100 because that's
+    // how Photoshop represents those values. Internally, however, the
+    // saturation and value are calculated from a range of 0 to 1. We make
+    // That conversion here.
+    s /= 100;
+    v /= 100;
+
+    if(s == 0) {
+      // Achromatic (grey)
+      r = g = b = v;
+      return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
     }
-    return hash;
-}
 
-function hsvToRgb(h, s, v) {
-  var r, g, b;
-  var i;
-  var f, p, q, t;
+    h /= 60; // sector 0 to 5
+    i = Math.floor(h);
+    f = h - i; // factorial part of h
+    p = v * (1 - s);
+    q = v * (1 - s * f);
+    t = v * (1 - s * (1 - f));
 
-  // Make sure our arguments stay in-range
-  h = Math.max(0, Math.min(360, h));
-  s = Math.max(0, Math.min(100, s));
-  v = Math.max(0, Math.min(100, v));
+    switch(i) {
+      case 0:
+        r = v;
+        g = t;
+        b = p;
+        break;
 
-  // We accept saturation and value arguments from 0 to 100 because that's
-  // how Photoshop represents those values. Internally, however, the
-  // saturation and value are calculated from a range of 0 to 1. We make
-  // That conversion here.
-  s /= 100;
-  v /= 100;
+      case 1:
+        r = q;
+        g = v;
+        b = p;
+        break;
 
-  if(s == 0) {
-    // Achromatic (grey)
-    r = g = b = v;
+      case 2:
+        r = p;
+        g = v;
+        b = t;
+        break;
+
+      case 3:
+        r = p;
+        g = q;
+        b = v;
+        break;
+
+      case 4:
+        r = t;
+        g = p;
+        b = v;
+        break;
+
+      default: // case 5:
+        r = v;
+        g = p;
+        b = q;
+    }
+
     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
   }
 
-  h /= 60; // sector 0 to 5
-  i = Math.floor(h);
-  f = h - i; // factorial part of h
-  p = v * (1 - s);
-  q = v * (1 - s * f);
-  t = v * (1 - s * (1 - f));
-
-  switch(i) {
-    case 0:
-      r = v;
-      g = t;
-      b = p;
-      break;
-
-    case 1:
-      r = q;
-      g = v;
-      b = p;
-      break;
-
-    case 2:
-      r = p;
-      g = v;
-      b = t;
-      break;
-
-    case 3:
-      r = p;
-      g = q;
-      b = v;
-      break;
-
-    case 4:
-      r = t;
-      g = p;
-      b = v;
-      break;
-
-    default: // case 5:
-      r = v;
-      g = p;
-      b = q;
+  function rgbToHex(r, g, b) {
+      return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
 
-  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
+  function roll20_colourise() {
+    $('#textchat .content .message.general:not(.you, .roll20-colourised)').each(function (i, node) {
+      var self = $(node);
+      var name = self.find('.by').text();
+      if (name) {
+        var c = hsvToRgb(Math.abs(hashCode(name)) % 360, 7, 93);
+        var hex = rgbToHex(c[0], c[1], c[2]);
+      } else {
+        var hex = self.prev().css('background-color');
+      }
+      self.css('background-color', hex);
+      self.addClass('roll20-colourised');
+    });
 
-function rgbToHex(r, g, b) {
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-function roll20_colourise() {
-  $('#textchat .content .message.general:not(.you, .roll20-colourised)').each(function (i, node) {
-    var self = $(node);
-    var name = self.find('.by').text();
-    if (name) {
-      var c = hsvToRgb(Math.abs(hashCode(name)) % 360, 7, 93);
-      var hex = rgbToHex(c[0], c[1], c[2]);
-    } else {
-      var hex = self.prev().css('background-color');
-    }
-    self.css('background-color', hex);
-    self.addClass('roll20-colourised');
-  });
+    window.setTimeout(roll20_colourise, 100);
+  }
 
   window.setTimeout(roll20_colourise, 100);
-}
-
-roll20_colourise();
+} + ')();';
+var script = document.createElement('script');
+script.textContent = actualCode;
+(document.head||document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
